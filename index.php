2c99c3d5ccce770ec8ccaed5f6cbc264bb7836dd
naxymails@gmail.com <naxymails@gmail.com>
2025-02-14 12:58:18 +0300

    start

---
+++ index.php
@@ -0,0 +1,10 @@
+<?
+require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php');
+$APPLICATION->SetTitle('Главная');
+?>
+
+<h1>Всем привет!</h1>
+
+<?
+require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php');
+?>
